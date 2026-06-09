2ae14397257ba03fc462881a468076119302f882
Bryan Lawrence <bryan.lawrence@ncas.ac.uk>
2026-06-09 15:47:34 +0100

    Starting a clean up

--- xconv2/xconv_cf_interface.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible import shim for xconv2.xconvcf."""
-
-from __future__ import annotations
-
-from .xconvcf import *  # noqa: F401,F403
-from .xconvcf import __all__ as _XCONVCF_ALL
-
-__all__ = _XCONVCF_ALL
